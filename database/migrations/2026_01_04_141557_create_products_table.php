42073c5b2a16b39d842776af89a402a495e7f680
Inbisat Naveed <inkyn2002@gmail.com>
2026-01-04 15:41:41 +0100

    Add products table migration

--- database/migrations/2026_01_04_141557_create_products_table.php
+++ database/migrations/2026_01_04_141557_create_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description')->nullable();
-            $table->decimal('price', 8, 2);
+            $table->decimal('price', 10, 2);
             $table->integer('stock')->default(0);
             $table->string('material')->nullable();
             $table->string('color')->nullable();
