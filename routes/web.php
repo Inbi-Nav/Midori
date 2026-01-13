cabfecc5081de193cca43f6eb2be99f2ebda046b
Inbisat Naveed <inkyn2002@gmail.com>
2026-01-13 13:22:39 +0100

    refactor: adjust home and dashboard routing

--- routes/web.php
+++ routes/web.php
@@ -3,23 +3,26 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ShopController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    if (!Auth::check()) {
+        return redirect()->route('login');
+    }
+
+    return app(ShopController::class)->index();
+})->name('shop.index');
+
+Route::get('/products/{product}', [ShopController::class, 'show'])->name('shop.show');
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    return redirect()->route('shop.index');
+})->middleware(['auth'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::get('/', [ShopController::class, 'index'])->name('shop.index');
-Route::get('/products/{product}', [ShopController::class, 'show'])->name('shop.show');
-
 
 require __DIR__.'/auth.php';
